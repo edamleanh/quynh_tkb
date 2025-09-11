3eddbfeb9a6d8a374d0bf9b590ee0f20bbbadd66
Edam Le <edamleanh@gmail.com>
2025-09-11 21:15:47 +0700

    fix: sửa SONTUNGMTP

--- src/App.jsx
+++ src/App.jsx
@@ -136,10 +136,11 @@ export default function App() {
 
   // 12 màu thẻ riêng cho giáo viên
   const TEACHER_COLORS = [
-    "bg-blue-100 text-blue-700",
+    "bg-pink-100 text-pink-700",
     "bg-green-100 text-green-700",
     "bg-amber-100 text-amber-700",
     "bg-purple-100 text-purple-700",
+    "bg-blue-100 text-blue-700",
     "bg-pink-100 text-pink-700",
     "bg-red-100 text-red-700",
     "bg-cyan-100 text-cyan-700",
